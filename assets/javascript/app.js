1e5e804240107ef3072143f5622a6352eaad1dd6
Rebecca Bartl <rebecca.bartl@gmail.com>
2019-09-02 19:01:36 -0500

    initialize firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,15 @@
-alert("hey you");
+// Initialize firebase
+var firebaseConfig = {
+    apiKey: "AIzaSyBtl-4ABkUyvur6ReXllCNGTDGqYHM3abI",
+    authDomain: "train-time-337eb.firebaseapp.com",
+    databaseURL: "https://train-time-337eb.firebaseio.com",
+    projectId: "train-time-337eb",
+    storageBucket: "train-time-337eb.appspot.com",
+    messagingSenderId: "873244974710",
+    appId: "1:873244974710:web:0d9ac58094fbc5b0"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
+
+  // Create a variable reference to the database
+  var database = firebase.database();
