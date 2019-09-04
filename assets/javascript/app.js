3e55458ac199c629b7775ee5edaf0441f251ea61
Rebecca Bartl <rebecca.bartl@gmail.com>
2019-09-04 11:32:05 -0500

    javascript/firebase work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,6 +14,7 @@ var firebaseConfig = {
   // Create a variable reference to the database
   var database = firebase.database();
 
+  // Click function for adding trains and getting user input
   $('#submit').on('click', function(){
 
     var trainName = $('#train-name').val().trim();
@@ -21,3 +22,20 @@ var firebaseConfig = {
     var firstTrainTime = moment($('#first-train-time').val().trim(), "HH:mm").format("X");
     var frequency = $('#frequency').val().trim();
   })
+
+  // Create "temporary" object for holding train data
+  var newTrain = {
+      name: trainName,
+      destination: destination,
+      firstTrain: firstTrainTime,
+      frequency: frequency,
+  };
+
+  // Pushes train data to the database
+  database.ref().push(newTrain);
+
+  // Clear the text boxes
+  $('#train-name').val("");
+  $('#destination-input').val("");
+  $('#first-train-time').val("");
+  $('#frequency').val("");
